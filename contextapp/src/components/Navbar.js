e1efb56d82dea6d2087d5877346d5c532387b426
pdavisjones <pdavisjones@gmail.com>
2020-01-13 09:53:59 -0600

    detailed use of consuming multiple contexts in a single component

--- contextapp/src/components/Navbar.js
+++ contextapp/src/components/Navbar.js
@@ -2,29 +2,42 @@
 
 import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
 
 class Navbar extends Component {
 
     render() { 
         return (
-        /** Instantiates Context Consumer */
-        <ThemeContext.Consumer>{(context) => {
-            /** Destructures data received from ThemeContext. */
-            const { isLightTheme, light, dark } = context;
-            /** Determines which theme is being used, then assignes it to the theme variable. */
-            const theme = isLightTheme ? light : dark;
-            /** Dynamically styles the Navbar. */
-            return (
-                <nav style={{ background: theme.ui, color: theme.syntax }}>
-                <h1>Context App</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-                </nav>
-            )
-        }}</ThemeContext.Consumer>
+          /** Instantiates AuthContext Consumer. Note: authContext value name is arbitrary. */
+          <AuthContext.Consumer>
+            {authContext => (
+              /** Instantiates ThemeContext Consumer. Note: themeContext name is arbitrary. */
+              <ThemeContext.Consumer>
+                {themeContext => {
+                  /** Destructures data and function consumed from AuthContext */
+                  const { isAuthenticated, toggleAuth } = authContext;
+                  /** Destructures data received from ThemeContext. */
+                  const { isLightTheme, light, dark } = themeContext;
+                  /** Determines which theme is being used, then assignes it to the theme variable. */
+                  const theme = isLightTheme ? light : dark;
+                  /** Dynamically styles the Navbar. */
+                  return (
+                    <nav style={{ background: theme.ui, color: theme.syntax }}>
+                      <h1>Context App</h1>
+                      <div onClick={toggleAuth}>
+                          { isAuthenticated ? 'Logged in' : 'Logged out' }
+                      </div>
+                      <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                      </ul>
+                    </nav>
+                  );
+                }}
+              </ThemeContext.Consumer>
+            )}
+          </AuthContext.Consumer>
         );
     }
 }
--- contextapp/src/contexts/AuthContext.js
+++ contextapp/src/contexts/AuthContext.js
@@ -3,13 +3,13 @@ import React, { Component, createContext } from 'react';
 export const AuthContext = createContext();
 
 class AuthContextProvider extends Component {
-    state = { isAuthenticated: false }
+    state = { isAuthenticated: true }
     toggleAuth = () => {
         this.setState({ isAuthenticated: !this.state.isAuthenticated })
     }
     render() { 
         return ( 
-            <AuthContext.Provider value={{...this.state, toggleAUth: this.toggleAUth}}>
+            <AuthContext.Provider value={{...this.state, toggleAuth: this.toggleAuth}}>
             { this.props.children }
             </AuthContext.Provider> 
          );
