e82aa08ee2082d633dfe0577366ccc445df2edb1
pdavisjones <pdavisjones@gmail.com>
2020-01-14 19:09:57 -0600

    leverage useEffect to store state in local storage

--- booklist-project/src/contexts/BookContext.js
+++ booklist-project/src/contexts/BookContext.js
@@ -1,14 +1,24 @@
 /** Imports the parent datasource for books from the reducer and instantiates methods for modifying it. Consumer Components will consume it using the useContext() method, destructuring what it needs to function in the left-hand side of the instantiation.  */
 
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 import { bookReducer } from '../reducers/BookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-    /** Stores books and dispatch method in this Component's state. */
-    const [books, dispatch] = useReducer(bookReducer, []);
+    /** Stores books and dispatch method in this Component's state. 
+    * Note: The third argument to useReducer determines what to set as the default value.
+    */
+    const [books, dispatch] = useReducer(bookReducer, [], () => {
+        const localData = localStorage.getItem('books');
+        return localData ? JSON.parse(localData) : [];
+    });
     
+    /** Stores user's input in local storage. */
+    useEffect(() => {
+        localStorage.setItem('books', JSON.stringify(books));
+    }, [books])
+
     /** Returns Context Provider */
     return (
         /** Passes Context data and methods to consuming Components. */
